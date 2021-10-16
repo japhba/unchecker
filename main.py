a515c7a504d95cf2c2c37164fb79042bbfea5be6
Jan Bauer <japhba@users.noreply.github.com>
2021-10-16 15:17:50 +0200

    optimized command line interface

--- main.py
+++ main.py
@@ -13,65 +13,46 @@ import img2pdf
 import tempfile
 import os
 from PIL import Image
+import json
 
 from pikepdf import Pdf, PdfImage, Name
 
 import argparse
 
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, Button
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import matplotlib
 matplotlib.use('Qt5Agg')
 
+# global default settings
+downscale = 0.3
+scaleFactor = 3
+gamma = 2
+maxpeaks = 100
+gaussBG = 10.0
+gaussMain = 3.0
 
-try:
-    parser = argparse.ArgumentParser(description='An fft-based grid removal filter')
-    parser.add_argument('input')
-    parser.add_argument('output')
-    parser.add_argument('--downscale', '-d', default=92, type=int,
-                        help='Threshold level for normalized magnitude spectrum')
-    parser.add_argument('--scaleFactor', '-r', default=6, type=int,
-                        help='Radius to expand the area of mask pixels')
-    parser.add_argument('--maxpeaks', '-m', default=4, type=int,
-                        help='Ratio for middle preservation')
-    parser.add_argument('--gamma', '-m', default=1.0, type=float,
-                        help='Ratio for middle preservation')
+dry_run = True
+input_fn = "script_qtcm_1s.pdf"
+use_settings = False
 
-    parser.add_argument('--dry_run', '-d', default=True, type=bool,
-                        help='Ratio for middle preservation')
-
-    args = parser.parse_args()
-    input_fn = args.input
-    output_fn = args.ouput
-    downscale = args.downscale
-    scaleFactor = args.scaleFactor
-    gamma = args.gamma
-    maxpeaks = args.maxpeaks
-    debug = args.debug
-    dry_run = args.dry_run
-
-except:
-    # global settings
-    downscale = 0.5
-    scaleFactor = 3
-    gamma = 2
-    maxpeaks = 300
-    dry_run = True
-    input_fn = "script_qtcm_1s.pdf"
 
 from pikepdf import Pdf, PdfImage, Name
 import zlib
 
 def pdfToImgs(filename, tmpdir=None):
+    print("Opening PDF...")
     pdf = Pdf.open(filename)
-
     page = pdf.pages[0]
-    image_name, rawimage = next(page.images.items())
+    # image_name, rawimage = next(page.images.items())
 
     imgs = []
 
-    for i, page in enumerate(pdf.pages):
+    n_pages = len(pdf.pages)
+
+    print("Extracting pages from PDF...")
+    for i, page in enumerate(tqdm(pdf.pages)):
         image_name, rawimage = next(page.images.items())
 
         pdfimage = PdfImage(rawimage)
@@ -87,7 +68,7 @@ def pdfToImgs(filename, tmpdir=None):
 
         imgs.append(img)
 
-    return imgs
+    return imgs, n_pages
 
 
 def imgsToPdf(imgs, filename, tmpdir):
@@ -111,8 +92,15 @@ class Pipeline():
         self.gamma = gamma
         self.maxpeaks = maxpeaks
 
-        self.gaussBG = 1.0
-        self.gaussMain = 1.0
+        self.gaussBG = gaussBG
+        self.gaussMain = gaussMain
+
+        if use_settings:
+            self.loadSettings()
+
+    @property
+    def settings(self):
+        return {"downscale": self.downscale, "scaleFactor": self.scaleFactor, "maxpeaks": self.maxpeaks, "gamma": self.gamma, "gaussBG": self.gaussBG, "gaussMain": self.gaussMain}
 
     def rescale(self, downscale=None):
         img = self.o_im
@@ -141,23 +129,30 @@ class Pipeline():
         return self.f_im
 
     def gaussFilter(self, scaleFactor=None, gaussMain=None, gaussBG=None):
+        """
+        Scales down the FFT spectrum after applying a Gauss filter to facilitate peakfinding.
+        :param scaleFactor:
+        :param gaussMain:
+        :param gaussBG:
+        :return:
+        """
 
         if not scaleFactor:
             scaleFactor = self.scaleFactor
         else:
-            scaleFactor = int(scaleFactor)
+            scaleFactor = (scaleFactor)
             self.scaleFactor = scaleFactor
             
         if not gaussBG:
             gaussBG = self.gaussBG
         else:
-            gaussBG = int(gaussBG)
+            gaussBG = (gaussBG)
             self.gaussBG = gaussBG
             
         if not gaussMain:
             gaussMain = self.gaussMain
         else:
-            gaussMain = int(gaussMain)
+            gaussMain = (gaussMain)
             self.gaussMain = gaussMain
 
         im = self.f_im
@@ -168,8 +163,8 @@ class Pipeline():
         g_im_bg = gaussian_filter(np.log(im), sigma=10 * downscale * gaussBG)
 
         x, y = g_im.shape
-        g_im = cv2.resize(g_im, dsize=(y // scaleFactor, x // scaleFactor), interpolation=cv2.INTER_AREA)
-        g_im_bg = cv2.resize(g_im_bg, dsize=(y // scaleFactor, x // scaleFactor), interpolation=cv2.INTER_AREA)
+        g_im = cv2.resize(g_im, dsize=(int(y // scaleFactor), x // int(scaleFactor)), interpolation=cv2.INTER_AREA)
+        g_im_bg = cv2.resize(g_im_bg, dsize=(int(y // scaleFactor), x // int(scaleFactor)), interpolation=cv2.INTER_AREA)
 
 
         self.g_im = g_im
@@ -182,6 +177,8 @@ class Pipeline():
         else:
             maxpeaks = int(maxpeaks)
             self.maxpeaks = maxpeaks
+
+        scaleFactor = self.scaleFactor
         
         # choose coordinates
         def x2u(x, ax=None):
@@ -196,6 +193,8 @@ class Pipeline():
 
         xx2u = lambda x, ax=None: x2u(x * scaleFactor, ax)
         u2xx = lambda u, ax=None: u2x(u, ax) // scaleFactor
+
+        # get the current, downsampled FFT spectrum
         g_im = self.g_im
         print("Begin thrshd", time.process_time())
         thrshd = photutils.detection.detect_threshold(g_im, nsigma=nsigma_trshd, background=self.g_im_bg)
@@ -204,7 +203,7 @@ class Pipeline():
         print("Begin resizing maps", time.process_time())
 
         print("Begin peakfinder", time.process_time())
-        detected_peaks = photutils.detection.find_peaks(g_im, box_size=u2xx(0.05), threshold=thrshd, npeaks=maxpeaks)
+        detected_peaks = photutils.detection.find_peaks(g_im, box_size=u2xx(0.04), threshold=thrshd, npeaks=maxpeaks)
 
         peaks = np.column_stack((detected_peaks["x_peak"][:], detected_peaks["y_peak"][:]))
 
@@ -213,7 +212,8 @@ class Pipeline():
             x, y = point[0] * scaleFactor, point[1] * scaleFactor
 
             # exclude peaks that are close to the center DC mode of the image
-            if abs(x - self.im.shape[1] / 2) < u2x(0.05, 1) and abs(y - self.im.shape[0] / 2) < u2x(0.05, 0):
+            centerTolerance = 0.01
+            if abs(x - self.im.shape[1] / 2) < u2x(centerTolerance, 1) and abs(y - self.im.shape[0] / 2) < u2x(centerTolerance, 0):
                 continue
 
             legit_peaks.append((x, y))
@@ -222,11 +222,12 @@ class Pipeline():
             xx, yy = np.meshgrid(np.arange(im.shape[1]), np.arange(im.shape[0]))
             x = np.stack((xx, yy), axis=-1)
             gauss = multivariate_normal.pdf(x, position, sigma)
-            gauss = gauss / np.max(gauss)
+            gauss = gauss / (np.max(gauss) + 0.0001)
 
             return gauss
 
-        notch = np.sum([notchMap(self.f_im, peak, sigma=u2x(0.03, 0)) for peak in legit_peaks[:]], axis=0)
+        # TODO not
+        notch = np.sum([notchMap(self.f_im, peak, sigma=u2x(0.08, 0)) for peak in legit_peaks[:]], axis=0)
         # clip close maxima
         notch = np.clip(notch, 0, 1)
         # invert
@@ -339,6 +340,22 @@ class Pipeline():
         self.backtransform()
         self.postProcess()
 
+    def saveSettings(self):
+        settings = self.settings
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.json'), 'w') as f:
+            json.dump(settings, f)
+
+    def loadSettings(self):
+        try:
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.json'), 'r') as f:
+                settings = json.load(f)
+
+            self.settings.update(settings)
+            for k, v in settings.items():
+                setattr(self, k, v)
+
+        except FileNotFoundError("Settings from dry-run not found. Using default settings instead!"):
+            return
 
 def batchFilter(tmpdir):
     _, _, imgs = next(os.walk(os.path.join(tmpdir, "pre")))
@@ -365,7 +382,6 @@ def split_list(alist, wanted_parts=1):
 
 
 
-
 def run(dry_run):
 
     if not dry_run:
@@ -382,48 +398,80 @@ def run(dry_run):
         tmpdir = tempfile.TemporaryDirectory(dir=pardir)
         os.makedirs(os.path.join(tmpdir.name, "pre"), exist_ok=True)
         os.makedirs(os.path.join(tmpdir.name, "post"), exist_ok=True)
-        imgs = pdfToImgs(filename, tmpdir.name)
+        imgs, n_pages = pdfToImgs(filename, tmpdir.name)
 
         filteredImgs = batchFilter(tmpdir.name)
 
+        print(output_fn)
         imgsToPdf(filteredImgs, output_fn, tmpdir.name)
 
         tmpdir.cleanup()
 
     else:
-        imgs = pdfToImgs(input_fn, None)
+        imgs, n_pages = pdfToImgs(input_fn, None)
 
         p = Pipeline(imgs[0])
 
         import matplotlib.pyplot as plt
         fig, ((ax1, ax2, ax3, ax3a), (ax4, ax5, ax5a, ax6)) = plt.subplots(2, 4, figsize=(10, 5))
 
-        plt.subplots_adjust(wspace=0, hspace=0.5)
+        plt.subplots_adjust(wspace=0, hspace=0.8)
 
         def draw_all():
             interpolation = "bilinear"
             ax1.imshow(p.im, cmap="gray", interpolation=interpolation)
-            ax2.imshow(np.log(p.f_im + 1e-1), cmap="gray", interpolation=interpolation)
-            ax3.imshow(p.g_im, interpolation=interpolation)
+            im2 = ax2.imshow(np.log(p.f_im + 1e-1), cmap="gray", interpolation=interpolation)
+            im3 = ax3.imshow(p.g_im, interpolation=interpolation)
+            vmin, vmax = im3.get_clim()
+
             ax3a.imshow(p.g_im_bg, interpolation=interpolation)
-            ax4.imshow(p.notch, interpolation=interpolation)
-            ax5.imshow(np.log(p.denotched_f + 1e-1), cmap="gray", interpolation=interpolation)
+            ax4.imshow(1-p.notch, interpolation=interpolation, cmap="Reds")
+            im5 = ax5.imshow(np.log(p.denotched_f + 1e-1), interpolation=interpolation, vmin=vmin, vmax=vmax)
             ax5a.imshow(np.log(p.im_filtered + 1e-1), cmap="gray", interpolation=interpolation)
             ax6.imshow(p.result, cmap="gray", interpolation=interpolation)
 
+            ax1.set_title("Input")
+            ax2.set_title("FFT spectrum")
+            ax3.set_title("Denoised spectrum")
+            ax3a.set_title("Background spectrum")
+
+            ax4.set_title("Detected peaks")
+            ax5.set_title("Cleaned spectrum")
+            ax5a.set_title("Backtransformed image")
+            ax6.set_title("Postprocessed image")
+
+            for ax in plt.gcf().get_axes():
+                ax.tick_params(top = False, bottom=False)
+                ax.set_xticklabels([])
+
             fig.canvas.draw_idle()
 
+        def refreshETA(s, e):
+            time_page = e - s
+            time_document = time_page * n_pages
+            fig.suptitle(f"ETA: {time_page:.2f}s per page, \n {int(time_document // 60)} min:{int(time_document % 60)}s for entire document")
+
         def downscale_fn(downscale):
+            start = time.process_time()
             p.rescale(downscale)
             p.fft()
             p.gaussFilter()
             p.findPeaks()
             p.backtransform()
             p.postProcess()
+            end = time.process_time()
+
+            refreshETA(start, end)
             draw_all()
 
+        # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+        resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+        button = Button(resetax, 'Save settings', hovercolor='0.975')
 
-        axcolor = 'lightgoldenrodyellow'
+        def saveSettings(event):
+            p.saveSettings()
+
+        button.on_clicked(saveSettings)
 
         divider = make_axes_locatable(ax1)
         ax_downscale = divider.append_axes("bottom", size="5%", pad=.05)
@@ -439,11 +487,15 @@ def run(dry_run):
 
 
         def downscale_peakfinder(scaleFactor):
+            start = time.process_time()
             p.gaussFilter(scaleFactor)
             p.findPeaks()
             p.backtransform()
             p.postProcess()
+            end = time.process_time()
+            refreshETA(start, end)
             draw_all()
+
 
         divider = make_axes_locatable(ax2)
         ax_scaleFactor = divider.append_axes("bottom", size="5%", pad=.05)
@@ -457,10 +509,59 @@ def run(dry_run):
 
         scaleFactor_slider.on_changed(downscale_peakfinder)
 
+        def set_gauss_main(gaussMain):
+            start = time.process_time()
+            p.gaussFilter(gaussMain=gaussMain)
+            p.findPeaks()
+            p.backtransform()
+            p.postProcess()
+            end = time.process_time()
+            refreshETA(start, end)
+            draw_all()
+
+
+        divider = make_axes_locatable(ax3)
+        ax_scaleFactor = divider.append_axes("bottom", size="5%", pad=.05)
+        gaussMain_slider = Slider(
+            ax=ax_scaleFactor,
+            label='GaussMain',
+            valmin=1,
+            valmax=10,
+            valinit=p.gaussMain,
+        )
+
+        gaussMain_slider.on_changed(set_gauss_main)
+
+        def set_gauss_bg(gaussBG):
+            start = time.process_time()
+            p.gaussFilter(gaussBG=gaussBG)
+            p.findPeaks()
+            p.backtransform()
+            p.postProcess()
+            end = time.process_time()
+            refreshETA(start, end)
+            draw_all()
+
+
+        divider = make_axes_locatable(ax3a)
+        ax_scaleFactor = divider.append_axes("bottom", size="5%", pad=.05)
+        gaussBG_slider = Slider(
+            ax=ax_scaleFactor,
+            label='GaussBG',
+            valmin=1,
+            valmax=30,
+            valinit=p.gaussBG,
+        )
+
+        gaussBG_slider.on_changed(set_gauss_bg)
+
         def on_maxpeaks(maxpeaks):
+            start = time.process_time()
             p.findPeaks(maxpeaks)
             p.backtransform()
             p.postProcess()
+            end = time.process_time()
+            refreshETA(start, end)
             draw_all()
 
         divider = make_axes_locatable(ax4)
@@ -481,21 +582,71 @@ def run(dry_run):
 
         divider = make_axes_locatable(ax6)
         ax_gamma = divider.append_axes("bottom", size="5%", pad=.05)
-        scaleFactor_slider = Slider(
+        gamma_slider = Slider(
             ax=ax_gamma,
             label='gamma',
             valmin=0.1,
-            valmax=10,
+            valmax=5,
             valinit=p.gamma,
         )
 
-        scaleFactor_slider.on_changed(on_gamma)
+        gamma_slider.on_changed(on_gamma)
 
 
         # run pipeline
+        s = time.process_time()
         p.full_pipeline()
+        e = time.process_time()
+        refreshETA(s, e)
         draw_all()
+        # plt.tight_layout()
 
         plt.show()
 
-run(dry_run)
+
+### main code
+try:
+    parser = argparse.ArgumentParser(description='An fft-based grid removal filter')
+    parser.add_argument('--input', '-i', default="", type=str,
+                        help='Input file path. ')
+
+    parser.add_argument('--output', '-o', default="", type=str,
+                        help='Output file path. ')
+
+    parser.add_argument('--downscale', '-d', default=downscale, type=float,
+                        help='Downsamples the PDF, given in percent. Choose as large as possible in terms of computational cost. ')
+    parser.add_argument('--res_peakfinder', '-r', default=scaleFactor**-1, type=float,
+                        help="Downsamples FFT spectrum on which the peakfinder acts, accelerating it. Doesn't affect output resolution. Choose as low as possible for optimal speed. ")
+    parser.add_argument('--maxpeaks', '-m', default=maxpeaks, type=int,
+                        help='Maximum number of peaks for peakfinder. Larger values give better grid removals at the cost of overall image degradation. ')
+    parser.add_argument('--gauss_main', '-gm', default=gaussMain, type=int,
+                        help='Kernel size to apply to the noisy FFT spectrum.  ')
+    parser.add_argument('--gauss_bg', '-gbg', default=maxpeaks, type=int,
+                        help='Kernel size to apply to estimate the background for the peakfinding algorithm.  ')
+
+    parser.add_argument('--gamma', '-g', default=gamma, type=float,
+                        help='Increases BW contrast in image in the end. Choose larger for higher contrast. ')
+
+    parser.add_argument('--no_dry_run', '-ndr', default=False, type=bool,
+                        help='Ratio for middle preservation. ')
+
+    parser.add_argument('--use_settings', '-u', default=False, type=bool,
+                        help='Use settings previously determined from dry-run. ')
+
+    args = parser.parse_args()
+
+    input_fn = args.input
+    output_fn = args.output
+    downscale = args.downscale
+    scaleFactor = int(downscale**(-1))
+    gamma = args.gamma
+    maxpeaks = args.maxpeaks
+    # debug = args.dry_
+    dry_run = args.dry_run
+    use_settings = args.use_settings
+    print(input_fn)
+    run(dry_run)
+
+except Exception as e:
+    print(e)
+    pass
---
+++ readme.md
@@ -0,0 +1,98 @@
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Thanks again! Now go create something AMAZING! :D
+***
+***
+***
+*** To avoid retyping too much info. Do a search and replace for the following:
+*** japhba, unchecker, twitter_handle, email, unchecker, FFT-based Python application to remove the grid in scanned, handwritten notes. 
+-->
+
+
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+<!--
+[![Contributors][contributors-shield]][contributors-url]
+[![MIT License][license-shield]][license-url]
+-->
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/japhba/unchecker">
+  </a>
+
+  <h3 align="center">unchecker</h3>
+
+  <p align="center">
+    A benchmark is a collection of functions, each individual function in a given benchmark is called a task. This repository contains some example methods as well as easily extendable base methods to create custom benchmarks. 
+    <br />
+  </p>
+</p>
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+Download the script and open a terminal at the location where the script is stored. 
+### Open the GUI for parameter adjustment
+```
+python main.py input my_griddy_file.pdf output my_blank_file.pdf
+```
+
+### Creating your own benchmark
+A benchmark that integrates well with the rest of the package can easily be implemented by inheriting from a base class. For parametric benchmarks, i.e. functions that can be constructed like y=f(x;p), with p a parameter, refer to ```base_parametric_benchmark.py``` and the annotations there. 
+For a non-parametric benchmark, e.g. consisting of samples of a Gaussian process, inherit from ```base_benchmark.py```. 
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+<!-- CONTACT -->
+## Contact
+
+Project Link: [https://github.com/japhba/unchecker](https://github.com/japhba/unchecker)
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/japhba/repo.svg?style=for-the-badge
+[contributors-url]: https://github.com/japhba/repo/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/japhba/repo.svg?style=for-the-badge
+[forks-url]: https://github.com/japhba/repo/network/members
+[stars-shield]: https://img.shields.io/github/stars/japhba/repo.svg?style=for-the-badge
+[stars-url]: https://github.com/japhba/repo/stargazers
+[issues-shield]: https://img.shields.io/github/issues/japhba/repo.svg?style=for-the-badge
+[issues-url]: https://github.com/japhba/repo/issues
+[license-shield]: https://img.shields.io/github/license/japhba/repo.svg?style=for-the-badge
+[license-url]: https://github.com/japhba/repo/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/japhba
